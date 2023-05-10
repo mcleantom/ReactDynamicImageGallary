b42fedda62652329ddf063f062059d1f9da2c430
Tom McLean <tom.mclean@bartechnologies.uk>
2023-05-10 22:08:09 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [state, setState] = useState({
+    showIndex: false,
+    showBullets: true,
+    infinite: true,
+    showThumbnails: true,
+    showFullscreenButton: true,
+    showGalleryFullscreenButton: true,
+    showPlayButton: true,
+    showGalleryPlayButton: true,
+    showNav: true,
+    isRTL: false,
+    slideDuration: 450,
+    slideInterval: 2000,
+    slideOnThumbnailOver: false,
+    thumbnailPosition: "bottom",
+    showVideo: {},
+    useWindowKeyDown: true,
+  });
+
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  // Fetch images from the public/images folder
+  const fetchImages = () => {
+    const images = [];
+    const imagesContext = require.context(
+      "../public/images",
+      false,
+      /\.(png|jpe?g|svg)$/
+    );
+
+    imagesContext.keys().forEach((imagePath) => {
+      images.push({
+        original: `/images/${imagePath.slice(2)}`,
+        thumbnail: `/images/${imagePath.slice(2)}`,
+      });
+    });
+
+    setImages(images);
+  };
+
+  // Fetch images on component mount
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchImages();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ImageGallery
+        items={images}
+        infinite={state.infinite}
+        showBullets={state.showBullets}
+        showFullscreenButton={
+          state.showFullscreenButton && state.showGalleryFullscreenButton
+        }
+        showPlayButton={state.showPlayButton && state.showGalleryPlayButton}
+        showThumbnails={state.showThumbnails}
+        showIndex={state.showIndex}
+        showNav={state.showNav}
+        isRTL={state.isRTL}
+        thumbnailPosition={state.thumbnailPosition}
+        slideDuration={parseInt(state.slideDuration)}
+        slideInterval={parseInt(state.slideInterval)}
+        slideOnThumbnailOver={state.slideOnThumbnailOver}
+        additionalClass="app-image-gallery"
+        useWindowKeyDown={state.useWindowKeyDown}
+      />
     </div>
   );
 }
